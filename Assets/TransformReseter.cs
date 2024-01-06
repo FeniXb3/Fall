f31b6b5a752d714c0970d25b0fe81f7e9488d295
Konrad Gadzina <konrad@makegames.today>
2024-01-06 16:56:00 +0100

    Reset rotation on pressing R

--- Assets/TransformReseter.cs
+++ Assets/TransformReseter.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TransformReseter : MonoBehaviour
 {
     public Vector3 startingPositon;
+    public Vector3 startingRotationAngles;
 
     // Start is called before the first frame update
     void Start()
     {
         startingPositon = transform.position;
+        startingRotationAngles = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class TransformReseter : MonoBehaviour
         {
             Debug.Log($"reset {name}");
             transform.position = startingPositon;
+            transform.rotation = Quaternion.Euler(startingRotationAngles);
         }
     }
 }
